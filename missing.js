6c8852ed9acad7895e0f1dd85e415b936049a9bd
hrs234 <itsharris892@gmail.com>
2019-10-06 19:44:04 +0700

    add first

---
+++ missing.js
@@ -0,0 +1,12 @@
+let num = "12346";
+console.log("maju");
+for (let init = 0; init < num.length; init++) {
+  console.log("hasil", parseInt(num[init]));
+}
+console.log("mundur");
+for (let tes = num.length + 1; tes > 0; tes--) {
+  console.log(tes);
+}
+// for (let tes = 0; num.length + 1 > tes; tes++) {
+//   console.log(tes);
+// }
